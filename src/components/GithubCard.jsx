a8a5a2e8f740cb458827242b19d8157bee4cd659
John Gustafson <John.Gustafson1@Marist.edu>
2021-02-18 09:31:03 -0500

    First commit

---
+++ src/components/GithubCard.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const GithubCard = () => {
+  return <div>I am Todo Item</div>
+}
+
+export default GithubCard;
---
+++ src/components/GithubCardList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import GithubCard from "./GithubCard";
+class ProjectList extends React.Component {
+  render() {
+    return <div><GithubCard /></div>
+  }
+}
+
+export default  ProjectList;
